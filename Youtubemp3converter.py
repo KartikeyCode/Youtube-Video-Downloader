47b6b48e304d176659459ca64d3d2baab3937b35
Kartikey <codekartikey@gmail.com>
2023-07-10 02:48:29 +0530

    add dist

--- Youtubemp3converter.py
+++ Youtubemp3converter.py
@@ -4,25 +4,13 @@ from tkinter.font import BOLD
 from pytube import YouTube
 import os
 
-#vid = YouTube(str(input("Enter URL of the youtube video \n =>")))
-#mp = int(input("Do you want MP3 or MP4 file? (3 for MP3, 4 for MP4)"))
-#print("Please Wait....")
-#if mp==4:
-#    yvideo = vid.streams.filter(file_extension='mp4').first()
-#    print("Downloading the video")
-#elif mp==3:
-#    yvideo = vid.streams.filter(only_audio=True).first()
-#    print("Downloading the audio")
-#else:
-#    print("ERROR: Input Not Recognised")
-#down = yvideo.download()
-
+current_directory = os.getcwd()
 window = Tk()
 window.title("Youtube to MP3/MP4")
 window.geometry('900x600')
-icon = PhotoImage(file='youtube.png')
-giticon = PhotoImage(file="github-logo.png")
-linkicon = PhotoImage(file="linkedin.png")
+icon = PhotoImage(file="D:\CODE\Youtube to mp3\youtube.png")
+giticon = PhotoImage(file="D:\CODE\Youtube to mp3\github-logo.png")
+linkicon = PhotoImage(file="D:\CODE\Youtube to mp3\linkedin.png")
 window.iconphoto(True,icon)
 window.config(background="black")
 alab = Label(window,text="Download Youtube Videos in MP3 or MP4 :D",font=('Arial',30,"bold"),fg='white',bg="black")
@@ -36,11 +24,11 @@ def submit():
     mp3down.place_forget()
 def fun_mp3():
     global mp3down
-    ylink.streams.filter(only_audio=True).first().download()
+    ylink.streams.filter(only_audio=True).first().download(output_path=current_directory)
     mp3down = Label(window,text="Downloaded Succesfully", font=("Arial",20,"italic"),fg="green",bg="black")
     mp3down.place(relx=0.32,rely=0.45)
 def fun_mp4():
-    ylink.streams.filter(file_extension='mp4').first().download()
+    ylink.streams.filter(file_extension='mp4').first().download(output_path=current_directory)
     mp3down.place(relx=0.32,rely=0.45)
 sub_but = Button(window,text="SUBMIT",command=submit)
 sub_but.place(relx=0.75,rely=0.11)
